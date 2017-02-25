413c993bc59c1f4ce72d43c22b948ccda5fb39ef
tugbagull0503 <gull.tugba@gmail.com>
2017-02-25 18:50:20 +0200

    First Commit

---
+++ MuzikCalar/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace MuzikCalar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lstbilgisayar = new System.Windows.Forms.ListView();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(458, 53);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(583, 361);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            this.axWindowsMediaPlayer1.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.axWindowsMediaPlayer1_PlayStateChange);
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader1});
+            this.listView1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(207)))), ((int)(((byte)(207)))));
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(0, 195);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(452, 219);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "url";
+            this.columnHeader3.Width = 0;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Radyolar";
+            this.columnHeader1.Width = 362;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 26);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Gözat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lstbilgisayar
+            // 
+            this.lstbilgisayar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.lstbilgisayar.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader2,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.lstbilgisayar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(207)))), ((int)(((byte)(207)))));
+            this.lstbilgisayar.FullRowSelect = true;
+            this.lstbilgisayar.Location = new System.Drawing.Point(1, 53);
+            this.lstbilgisayar.Name = "lstbilgisayar";
+            this.lstbilgisayar.Size = new System.Drawing.Size(451, 136);
+            this.lstbilgisayar.TabIndex = 5;
+            this.lstbilgisayar.UseCompatibleStateImageBehavior = false;
+            this.lstbilgisayar.View = System.Windows.Forms.View.Details;
+            this.lstbilgisayar.SelectedIndexChanged += new System.EventHandler(this.lstbilgisayar_SelectedIndexChanged);
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "#";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Parça Adı";
+            this.columnHeader4.Width = 250;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Konum";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox1.Location = new System.Drawing.Point(86, 21);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(366, 26);
+            this.textBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = global::MuzikCalar.Properties.Resources.arkaplan_backgrounds_muzik_music_4;
+            this.ClientSize = new System.Drawing.Size(1039, 414);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lstbilgisayar);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.MaximumSize = new System.Drawing.Size(1055, 453);
+            this.MinimumSize = new System.Drawing.Size(1055, 453);
+            this.Name = "Form1";
+            this.Text = "Müzik Calar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView lstbilgisayar;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ MuzikCalar/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using WMPLib;
+
+namespace MuzikCalar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        XmlDocument doc = new XmlDocument();
+        int sarkisayisi = 0;
+        int sira = 0;
+        int index;
+       
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            XmlRadyoGetir();
+        }
+
+      
+        /// <summary>
+        /// XML den radyoları ve MMS adreslerini çeken method.
+        /// </summary>
+        void XmlRadyoGetir()
+        {
+            //doc.Load("C:\\Users\\tugba\\Desktop\\radiodb.xml");
+            doc.Load(Application.StartupPath + "\\" + "radiodb.xml");
+            XmlElement root = doc.DocumentElement;
+            XmlNodeList kayitlar = root.SelectNodes("/data/row");
+
+            foreach (XmlNode secilen in kayitlar)
+            {
+                ListViewItem lv = new ListViewItem();
+                lv.Text = secilen.Attributes["url"].InnerText;
+                lv.SubItems.Add(secilen.Attributes["name"].InnerText);
+                listView1.Items.Add(lv);
+                
+            }
+        }     
+/// <summary>
+/// Radyonun çalınmasını sağlayan method.
+/// </summary>
+/// <param name="url"></param>
+        void RadyoCal(string url)
+        {
+            axWindowsMediaPlayer1.URL = url;
+          
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+
+            {
+                RadyoCal(listView1.SelectedItems[0].Text.ToString());
+
+            }
+            
+          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+                openFileDialog1.Filter = "Media File(*.mpg,*.dat,*.avi,*.wmv,*.wav,*.mp3)|*.wav;*.mp3;*.mpg;*.dat;*.avi;*.wmv"; //seçeceğimiz dosyalar arasında filtreleme yapar. 
+                //openFileDialog1.InitialDirectory = Application.StartupPath;  //Debug klasöründen başlanılmasını sağlıyor. 
+                openFileDialog1.Title = "Dosya Seç";
+                openFileDialog1.RestoreDirectory = true;//En son açılan dizi gözata basıldığı zaman tekrar açılır.
+                //openFileDialog1.ShowDialog();
+                openFileDialog1.CheckFileExists = false;
+                if (openFileDialog1.ShowDialog().ToString() == "OK") { 
+                //çoklu seçim için
+                string[] safefilenames = openFileDialog1.SafeFileNames;
+                string[] filenames = openFileDialog1.FileNames;
+                openFileDialog1.CheckFileExists = true;
+                openFileDialog1.CheckPathExists = true;
+
+                sira = lstbilgisayar.Items.Count; //listviewdaki kolonlari sayiyoruz.
+                for (int i = 0; i < filenames.Length; i++)
+                {
+                    sarkisayisi++;
+                    lstbilgisayar.Items.Add(sarkisayisi.ToString());
+                    lstbilgisayar.Items[i].SubItems.Add(safefilenames[i]);
+                    lstbilgisayar.Items[i].SubItems.Add(filenames[i]);
+
+                }
+
+                for (int i = 0; i < openFileDialog1.SafeFileNames.Length; i++)
+                {
+
+                    lstbilgisayar.Items[sira].SubItems.Add(openFileDialog1.SafeFileNames[i].ToString());
+
+                }
+                lstbilgisayar.Items[sira].SubItems.Add(openFileDialog1.FileName.ToString());
+                openFileDialog1.FileName = "";
+                }
+           
+        }
+        
+
+        private void lstbilgisayar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+                {
+            if (lstbilgisayar.SelectedItems.Count > 0)
+            {
+                index = lstbilgisayar.SelectedItems[0].Index;
+                RadyoCal(lstbilgisayar.Items[index].SubItems[2].Text);
+                textBox1.Text ="Oynatılıyor: "+ " " + lstbilgisayar.Items[index].SubItems[1].Text;
+
+            }
+            }
+                catch (Exception ex)
+                {
+
+                    lstbilgisayar.SelectedItems[0].Remove();
+                    //MessageBox.Show(ex.ToString());
+                }
+            
+        }
+
+        private void axWindowsMediaPlayer1_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsStopped)
+            {
+                WMPLib.IWMPPlaylist playlist = axWindowsMediaPlayer1.playlistCollection.newPlaylist("myplaylist");
+                WMPLib.IWMPMedia media;                                      
+                for (int i = 0; i < lstbilgisayar.Items.Count; i++)
+                {
+                        media = axWindowsMediaPlayer1.newMedia(lstbilgisayar.Items[i].SubItems[2].Text);
+                        playlist.appendItem(media);                  
+                }
+               
+                axWindowsMediaPlayer1.currentPlaylist = playlist;
+                axWindowsMediaPlayer1.settings.autoStart = true;
+                axWindowsMediaPlayer1.Ctlcontrols.next();
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+                }
+               
+            }
+       
+        }
+
+     
+        }
+    
+
